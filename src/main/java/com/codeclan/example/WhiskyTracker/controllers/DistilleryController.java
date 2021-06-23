f89e6d5bfac32013b02cb93d3acfdbb149a938d1
sailorj94 <jackniall9@gmail.com>
2021-06-23 16:54:35 +0100

    Basic functionality added to controllers

---
+++ src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -0,0 +1,34 @@
+package com.codeclan.example.WhiskyTracker.controllers;
+
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DistilleryController {
+
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
+    @GetMapping(value="/distilleries")
+    public ResponseEntity<List<Distillery>> getAllDistilleries(){
+        return new ResponseEntity<>(distilleryRepository.findAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/distillery/{id}")
+    public ResponseEntity getDistillery(@PathVariable Long id){
+        return new ResponseEntity<>(distilleryRepository.findById(id), HttpStatus.OK);
+    }
+
+
+}
